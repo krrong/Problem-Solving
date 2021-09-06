b3b01cced8b32fa50cef16bfeafc35dc3d044736
krrong <krrong>
2021-09-06 16:55:12 +0900

    BOJ 3273

--- CodingTest/21.09.06 BOJ 3273.cpp
+++ CodingTest/21.09.06 BOJ 3273.cpp
@@ -20,25 +20,34 @@ int main() {
 	// 오름차순 정렬
 	sort(v.begin(), v.end());
 
-	// aj의 j를 이분 탐색
+	// 각 항마다 이분 탐색 -> aj의 j를 찾는다.(inddex)
 	for (int i = 0; i < n - 1; i++) {
-		int fir = v[i];	// ai
+		int searchNum = x - v[i];	// 찾는 수
 		int left = i;
-		int right = n;
+		int right = n - 1;
 		int mid = 0;
 
 		while (left <= right) {
 			mid = (left + right) / 2;
 
-			if (fir + v[mid] > x) {
+			if (searchNum < v[mid]) {
 				right = mid - 1;
 			}
-			else {
+			else if (searchNum > v[mid]) {
 				left = mid + 1;
 			}
+			// 같다면 stop
+			else {
+				break;
+			}
+		}
+		
+		if (mid >= n) {
+			continue;
 		}
 
-		if (fir + v[left] == x) {
+		// mid 와 i가 같으면 같은 항이 두 번 사용될 수 있기 때문에 제외해줘야 한다.
+		if (v[mid] == searchNum && mid != i) {
 			result++;
 		}
 	}
