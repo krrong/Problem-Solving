b1c2c9b0e7f4988686a7e36e0dbb163129dd715d
krrong <krrong>
2021-09-11 16:40:34 +0900

    BOJ 15649 Backtracking

---
+++ CodingTest/21.09.11 BOJ 15649.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool visit[10];
+
+// 백트래킹 
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visit[i] == false) {
+			arr[k] = i;
+			visit[i] = true;
+			func(k + 1);
+			visit[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+	func(0);
+	
+}
